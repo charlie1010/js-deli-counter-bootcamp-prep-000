3c5140ffacb7c982a633683d15762a9543dad182
CHARLIE MCCORMICK <chamccor9692@gmail.com>
2018-05-11 20:48:56 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,13 @@
+var katzDeli = []
+
+function takeANumber(katzDeli,name) {
+  return `Welcome, ${name}. You are number ${katzDeli} in line.`
+}
+
+function nowServing() {
+  for or while with if statements 
+}
+
+function currentLine() {
+  for or while with if statment
+}
