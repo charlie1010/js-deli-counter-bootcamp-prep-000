6438e20ac9029c61c6a5e0a4fde2f246880e9af0
CHARLIE MCCORMICK <chamccor9692@gmail.com>
2018-05-11 21:29:08 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -15,20 +15,11 @@ if (katzDeli.length === 0) {
 }
 else
 return(`Currently serving ${katzDeli.shift()}.`)
-
-
-var line = [];
+}
 
 function currentLine(katzDeli) {
-  let i = 0
-  while (i < katzDeli.length) {
-    line.push(' '+[i+1]+'. ' + katzDeli[i])
-    i++
-  }
+for (let i = 0; i < katzDeli.length; i++) {
+line.push(` `+[i+1]+`. `  + katzDeli[i])
+ }
+return(`The line is currently:` + line);
 }
-  if (katzDeli.length === 0) {
-    return "The line is currently empty."
-   } 
-   else
-    return(`The line is currently:` + line)
-  }
