7bff2c57e26e9e5d98e855ee905dcd324d33e31e
CHARLIE MCCORMICK <chamccor9692@gmail.com>
2018-05-11 21:12:11 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -30,3 +30,16 @@ function currentLine(katzDeli) {
   } else
   return(`The line is currently:` + line);
 }
+
+function currentLine(line) {
+  let i = 0
+  while (i < katzDeli.length) {
+    line.push(' '+[i+1]+'. ' + katzDeli[i])
+    i++
+  }
+}
+  if (katzDeli.length === 0) {
+    return "The line is currently empty."
+   } else
+    return(`The line is currently:` + line)
+  }
